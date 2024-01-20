378eeab7b21e0044ac502484ee508a94e54a0d1c
Hyune-c <choi8608@gmail.com>
2024-01-21 05:53:59 +0900

    add api: content 만 반환하는 생성 요청 api 구현

--- src/main/kotlin/com/example/practicespringai/app/controller/OpenAiController.kt
+++ src/main/kotlin/com/example/practicespringai/app/controller/OpenAiController.kt
@@ -27,4 +27,21 @@ class OpenAiController(
         val prompt = Prompt(message)
         return streamingChatClient.generateStream(prompt)
     }
+
+    @GetMapping("/open-ai/generateStream/content-only")
+    fun generateStreamContentOnly(@RequestParam(defaultValue = "Tell me a joke") message: String): Flux<String> {
+        val prompt = Prompt(message)
+        return streamingChatClient.generateStream(prompt)
+            .filter { !it.isFinished() }
+            .map { it.generation.content }
+    }
+
+    /**
+     * content 가 null 또는 finishReason 이 STOP 인 경우 마지막 응답으로 간주
+     * TODO 정확한 문서는 찾지 못해 임의로 판단함
+     */
+    private fun ChatResponse.isFinished(): Boolean {
+        return this.generation.content == null
+            || this.generation.choiceMetadata.finishReason == "STOP"
+    }
 }
