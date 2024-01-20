366a853f12507713ff290a23bcbc065223c4bf61
Hyune-c <choi8608@gmail.com>
2024-01-21 04:35:02 +0900

    init

---
+++ src/main/kotlin/com/example/practicespringai/PracticeSpringAiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practicespringai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeSpringAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeSpringAiApplication>(*args)
+}
