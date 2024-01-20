8ef7d3f0c57badd5f4871d9f34b50ce9ba69ffe8
Hyune-c <choi8608@gmail.com>
2024-01-21 05:01:49 +0900

    add api: sync 생성 요청 api 추가

--- src/main/kotlin/com/example/practicespringai/app/controller/TestController.kt
+++ src/main/kotlin/com/example/practicespringai/app/controller/TestController.kt
@@ -1,12 +1,17 @@
 package com.example.practicespringai.app.controller
 
+import org.springframework.ai.chat.ChatClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestController {
-    @GetMapping("/test")
-    fun test(): String {
-        return "Hello World!"
+class TestController(private val chatClient: ChatClient) {
+    /**
+     * average response time: 2 seconds
+     */
+    @GetMapping("/ai/generate")
+    fun generate(@RequestParam(defaultValue = "Tell me a joke") message: String): Map<*, *> {
+        return mapOf("generation" to chatClient.generate(message))
     }
 }
