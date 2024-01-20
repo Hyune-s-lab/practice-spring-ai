85c6ee9a87137513da6a6be0a971e6bb9ce2712d
Hyune-c <choi8608@gmail.com>
2024-01-21 05:27:00 +0900

    rename api: controller 명 개선 & api path 개선

--- src/main/kotlin/com/example/practicespringai/app/controller/OpenAiController.kt
+++ src/main/kotlin/com/example/practicespringai/app/controller/OpenAiController.kt
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 
 @RestController
-class TestController(
+class OpenAiController(
     private val chatClient: ChatClient,
     private val streamingChatClient: StreamingChatClient,
 ) {
     /**
      * average response time: 2 seconds
      */
-    @GetMapping("/ai/generate")
+    @GetMapping("/open-ai/generate")
     fun generate(@RequestParam(defaultValue = "Tell me a joke") message: String): Map<*, *> {
         return mapOf("generation" to chatClient.generate(message))
     }
 
-    @GetMapping("/ai/generateStream")
+    @GetMapping("/open-ai/generateStream")
     fun generateStream(@RequestParam(defaultValue = "Tell me a joke") message: String): Flux<ChatResponse> {
         val prompt = Prompt(message)
         return streamingChatClient.generateStream(prompt)
